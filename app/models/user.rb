4201cf0293d8f85e95ec88379f6ba5acec6fcb4c
MarMinsk <mwheatley@hotmail.com>
2017-06-01 16:30:03 +0100

    authenticating register route

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
