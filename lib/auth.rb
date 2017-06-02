d3fc7e0986bf782d4fcca96655076fb7a6236ba7
MarMinsk <mwheatley@hotmail.com>
2017-06-02 11:44:34 +0100

    getting login & register auth controllers to work

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,13 +1,13 @@
 require 'jwt'
 
 class Auth
-  ALGORITHM = "H256"
+  ALGORITHM = "HS256"
   # self. binds these methods to this class
   def self.issue(payload, expiry_in_minutes=60*24*30)
     payload[:exp] = expiry_in_minutes.minutes.from_now.to_i
     JWT.encode(payload, auth_secret, ALGORITHM)
   end
-  
+
   def self.decode(token, leeway=0)
     decoded = JWT.decode(token, auth_secret, true, { leeway: leeway, algorithm: ALGORITHM })
     HashWithIndifferentAccess.new(decoded[0])
