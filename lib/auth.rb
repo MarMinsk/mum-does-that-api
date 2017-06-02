49f25f7474f865ded669cec51ddcecdf97471af5
MarMinsk <mwheatley@hotmail.com>
2017-06-02 09:43:04 +0100

    adding auth_secret

--- lib/auth.rb
+++ lib/auth.rb
@@ -3,11 +3,11 @@ require 'jwt'
 class Auth
   ALGORITHM = "H256"
   # self. binds these methods to this class
-  def self.issue(payload, expiry=24.hours.from_now)
-    payload[:exp] = expiry.to_i
+  def self.issue(payload, expiry_in_minutes=60*24*30)
+    payload[:exp] = expiry_in_minutes.minutes.from_now.to_i
     JWT.encode(payload, auth_secret, ALGORITHM)
   end
-
+  
   def self.decode(token, leeway=0)
     decoded = JWT.decode(token, auth_secret, true, { leeway: leeway, algorithm: ALGORITHM })
     HashWithIndifferentAccess.new(decoded[0])
