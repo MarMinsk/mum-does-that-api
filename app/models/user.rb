1ec45b42ce9270886fa84fb5102ef10fca5b97b5
MarMinsk <mwheatley@hotmail.com>
2017-06-05 15:32:53 +0100

    removing posts from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :posts
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   scope :api do
     resources :users
-    resources :posts
     post 'register', to: 'authentications#register'
     post 'login', to: 'authentications#login'
   end
