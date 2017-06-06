4e3383873fa8f7307ea1c008ea831387e513e6f1
MarMinsk <mwheatley@hotmail.com>
2017-06-06 19:52:58 +0100

    fixing serializer spelling mistake

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :email, :password_digest, :image, :bio, :locataion, :linkedin, :twitter, :company_name, :company_website, :company_logo, :company_profile
+  attributes :id, :username, :first_name, :last_name, :email, :password_digest, :image, :bio, :location, :linkedin, :twitter, :company_name, :company_website, :company_logo, :company_profile
 end
 
 
