2ed39c606c9be89f3df28fa41d361a580c3873c5
MarMinsk <mwheatley@hotmail.com>
2017-06-08 11:21:33 +0100

    updating tasks show images

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :email, :password_digest, :image, :bio, :location, :linkedin, :twitter, :company_name, :company_website, :company_logo, :company_profile
+  has_many :skills
 end
 
 
